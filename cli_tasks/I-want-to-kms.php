88f785ccdec55a3b3563c03118072c509328be73
your name <you@example.com>
2025-09-08 09:33:37 +0300

    numbers assigned to functions, user may see the list

--- cli_tasks/I-want-to-kms.php
+++ cli_tasks/I-want-to-kms.php
@@ -4,7 +4,7 @@
 // ok 2) izveidojam while ciklu, kas vaicā pēc lietotāja ievades (ar 'readline') katrā iterācijā (iziet ar Ctrl+C)
 // ok 3) while cikls tiek pārtraukts, ja lietotājs ievada "n"
 // ok 4) izveidojam uzdevumu (tasks) sarakstu kā String elementus izmantojot indeksēta masīva datu struktūru (3 testa elementus)
-// 5) izveidojiet "switch..case" konstrukciju, kas ļauj apstrādāt lietotāja ievadīto izvēli
+// ok 5) izveidojiet "switch..case" konstrukciju, kas ļauj apstrādāt lietotāja ievadīto izvēli
 // 6) pievienojiet 'case' jeb gadījumu '1', kuru ievadot lietotājam tiek izvadīts viss uzdevumu saraksts
 // 7) uzlabojiet šo 'case: 1' bloku, lai tas izsauktu funkciju, kas atgriež uzdevumus
 // 8) izvadiet lietotāja izvēlnes tekstu, kas to infomē par veicamajām darbībām CLI aplikācijā
@@ -22,7 +22,9 @@ while(true){
         case 0:
             exit;
         case 1:
-            echo "You have ahlelele'd one time";
+            foreach($tasks as $task){
+                echo $task ."\n";
+            }
             break;
     }
 }
