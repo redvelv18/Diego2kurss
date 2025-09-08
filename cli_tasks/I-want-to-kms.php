a44901dba09c3d0292c2d1d7660d80968a58a299
your name <you@example.com>
2025-09-08 08:49:09 +0300

    task list added

--- cli_tasks/I-want-to-kms.php
+++ cli_tasks/I-want-to-kms.php
@@ -1,4 +1,15 @@
 <?php
 
+// 1) izveidojam jaunu PHP projektu "Tasks" un versionējam
+// 2) izveidojam while ciklu, kas vaicā pēc lietotāja ievades (ar 'readline') katrā iterācijā (iziet ar Ctrl+C)
+// 3) while cikls tiek pārtraukts, ja lietotājs ievada "n"
+// 4) izveidojam uzdevumu (tasks) sarakstu kā String elementus izmantojot indeksēta masīva datu struktūru (3 testa elementus)
+// 5) izveidojiet "switch..case" konstrukciju, kas ļauj apstrādāt lietotāja ievadīto izvēli
+// 6) pievienojiet 'case' jeb gadījumu '1', kuru ievadot lietotājam tiek izvadīts viss uzdevumu saraksts
+// 7) uzlabojiet šo 'case: 1' bloku, lai tas izsauktu funkciju, kas atgriež uzdevumus
+// 8) izvadiet lietotāja izvēlnes tekstu, kas to infomē par veicamajām darbībām CLI aplikācijā
+// 9) izveidojiet izvēlni, kas ļauj lietotājam pievienot jaunu uzdevumu
+// 10) izveidojiet izvēlni, kas ļauj lietotājam dzēt eksistējošu uzdevumu
+
 
 echo "I don't wanna be here";
