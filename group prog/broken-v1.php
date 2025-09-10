dcbfda1879f84023323cc333615b8b8422f9de09
your name <you@example.com>
2025-09-10 14:58:52 +0300

    CRUD app with books fixed and working

---
+++ group prog/broken-v1.php
@@ -0,0 +1,88 @@
+<?php
+
+$books = [
+    1 => [
+        $id => '1',
+        'title' => 'The Great Gatsby',
+        'author' => 'F. Scott Fitzgerald'
+    ],
+    2 => [
+        $id => '2',
+        'title' => '1984',
+        'author' => 'George Orwell'
+    ],
+    3 => [
+        $id => '3',
+        'title' => 'Pride and Prejudice',
+        'author' => 'Jane Austen'
+    ]
+];
+
+
+function showAllBooks(&$books) {
+    foreach ($books as $id => $book) {
+        displayBook($id, $book);
+    }
+}
+
+function showBook(&$books) {
+    $id = readline("Enter book id: ");
+    displayBook($id, $books[$id]);
+}
+
+function addBook(&$books) {
+    $title = readline("Enter title: ");
+    $author = readline("Enter author: ");
+    $books[] = ['title' => $title, 'author' => $author];
+}
+
+function deleteBook(&$books) {
+    $id = readline("Enter book ID you want to delete: ");
+    if (isset($books[$id])) {
+        unset($books[$id]);
+        echo "Grāmata dzēsta\n";
+    } else {
+        echo "Grāmata nav atrasta\n";
+    }
+}
+
+function displayBook($id, $book) {
+    echo "ID:". $id. "// Title: ". $book['title'] . " // Author: " . $book['author']. "\n\n";
+}
+
+echo "\n\nWelcome to the Library\n";
+$continue = true; 
+do {
+    echo "\n\n";
+    echo "1 - show all books\n";
+    echo "2 - show a book\n";
+    echo "3 - add a book\n";
+    echo "4 - delete a book\n";
+    echo "5 - quit\n\n";
+    $choice = readline();
+
+    switch ($choice) {
+        case 1:
+            showAllBooks($books);
+            break;
+        case 2:
+            showBook($books);
+            break;
+        case 3:
+            addBook($books);
+            break;
+        case 4:
+            deleteBook($books);
+            break;
+        case 5:
+            echo "Goodbye!\n";
+            $continue = false;
+            break;
+        case 13:
+            print_r($books); // hidden option to see full $books content
+            break;
+        default:
+            echo "Invalid choice\n";
+    };
+
+} while ($continue == true);
